9db5801ed1547a6500724dd6aef1abf007c29560
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-11 21:20:23 -0300

    incrementando desafio

--- desafios/modelo de site 3/script.js
+++ desafios/modelo de site 3/script.js
@@ -2,10 +2,26 @@ function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         alert('[ERRO] Faltam dados!!')
     } else {
-        alert('Tudo ok.')
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+        // Contagem Crescente...
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // Contagem Regressiva...
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
