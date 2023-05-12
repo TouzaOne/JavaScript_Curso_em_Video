ba952ccfd010c1408f4cbaf4d8a48c90d6ccda27
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-12 09:28:59 -0300

    utilizando o laço While

--- desafios/modelo de site 4/script.js
+++ desafios/modelo de site 4/script.js
@@ -5,10 +5,12 @@ function tabuada() {
         alert('Por favor, digite um número!')
     } else {
         let n = Number(num.value)
-        let c = 1
+        let c = 0
+        tab.innerHTML = ''
         while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
