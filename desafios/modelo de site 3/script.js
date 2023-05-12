aa1f5ceadc374b8e1d63386a83d588caf9ba6fe9
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-11 21:29:49 -0300

    atualizando script

--- desafios/modelo de site 3/script.js
+++ desafios/modelo de site 3/script.js
@@ -11,6 +11,10 @@ function contar() {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo Inválido! Considerando Passo: 1')
+            p = 1
+        }
         if (i < f) {
         // Contagem Crescente...
             for (let c = i; c <= f; c += p) {
