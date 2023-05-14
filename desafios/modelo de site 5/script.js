78a833708f1d7c825a108841ffbf64782de2114a
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-14 13:30:59 -0300

    adicionando comandos no JS do desafio

--- desafios/modelo de site 5/script.js
+++ desafios/modelo de site 5/script.js
@@ -19,16 +19,44 @@ function inLista(n, l) {
     }
 }
 
-
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `Valor ${num.value}: adicionado com sucesso!`
+        item.text = `Número ${num.value}: adicionado com sucesso! `
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Valor digitado inválido ou já encontrado na lista!')
     }
     num.value = ''
     num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!!!')
+    } else {
+        let tot = valores.length
+        let menor = valores[0]
+        let maior = valores[0]
+        let soma = 0
+        let media = 0
+
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+        }
+        media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O menor número digitado foi ${menor}.</p>`
+        res.innerHTML += `<p>O maior número digitado foi ${maior}.</p>`
+        res.innerHTML += `<p>A soma de todos os valores digitados dá: ${soma}.</p>`
+        res.innerHTML += `<p>A média de todos os valores digitados é: ${media}.</p>`
+    }
 }
