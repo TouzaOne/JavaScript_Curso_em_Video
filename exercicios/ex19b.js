9b987032849e83a05dbe56f1dcae52ebcf875dec
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-14 10:47:31 -0300

    função no ex19b

---
+++ exercicios/ex19b.js
@@ -0,0 +1,14 @@
+// Função em JavaScript
+
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+/* Repare que quando eu passo, já logo no início, um valor para as minhas variáveis, todo o processo fica mais fácil. */
+
+console.log(soma(7, 3))
+
+console.log(soma(7))
+
+console.log(soma( 0, 3))
+
+console.log(soma())
