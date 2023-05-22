0c2baa51b7a030a684cc69ca79bc403abf50bb7d
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-22 10:05:35 -0300

    switch-case em JS

---
+++ JS_Andre_Iacono/switch_case.js
@@ -0,0 +1,15 @@
+let airport = 'JFK'
+
+switch (airport) {
+    case 'MCO':
+        console.log('Airport in Orlando')
+        break
+    case 'JFK':
+        console.log('Airport of John F. Kennedy')
+        break
+    case 'SEA':
+        console.log('Airport in Seattle')
+        break
+    default:
+        console.log('Airport Unknown')
+}
