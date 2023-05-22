19f2ddf977c57c90b8712df616bdfb07f0bbcc19
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-22 09:52:36 -0300

    if e else em JS

---
+++ JS_Andre_Iacono/if-else.js
@@ -0,0 +1,9 @@
+let driverSpeed = 110
+
+if (driverSpeed > 110) {
+    console.log('ALERT... Driving too fast!!')
+} else if (driverSpeed > 40 && driverSpeed <= 110) {
+    console.log('Ok!!')
+} else {
+    console.log('ALERT... Driving too slow!!')
+}
