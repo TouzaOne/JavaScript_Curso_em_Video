d4c8d560d3ae3eb4cc1a03c284125b85fb473466
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-22 10:24:25 -0300

    while em JS

---
+++ JS_Andre_Iacono/while.js
@@ -0,0 +1,16 @@
+
+let i = 1
+console.log('Em ordem crescente...')
+
+while(i <= 10){
+    console.log('Number ', i)
+    i++
+}
+
+let n = 10
+console.log('Em ordem decrescente...')
+
+while(n >= 1){
+    console.log('Number ', n)
+    n--
+}
