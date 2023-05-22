0907418d09cab8f375609dfc34b1ea435ad1a5ba
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-22 10:57:28 -0300

    adicionando break ao while

---
+++ JS_Andre_Iacono/while-com-break.js
@@ -0,0 +1,18 @@
+
+let i = 1
+console.log('Em ordem crescente...')
+
+while(i <= 10){ 
+    if (i === 7) break   
+    console.log('Number ', i)
+    i++
+}
+
+let n = 10
+console.log('Em ordem decrescente...')
+
+while(n >= 1){
+    if (n === 3) break
+    console.log('Number ', n)
+    n--
+}
