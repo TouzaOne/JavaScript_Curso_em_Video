831c6773d2f5f681db17ee19c3841de603794faa
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-22 10:16:44 -0300

    for loop em JS

---
+++ JS_Andre_Iacono/for.js
@@ -0,0 +1,11 @@
+console.log('Em ordem crescente...')
+
+for (i = 1; i <=10; i++){
+    console.log('Number ', i)
+}
+
+console.log('Em ordem decrescente...')
+
+for (n = 10; n >= 1; n--){
+    console.log('Number ', n)
+}
