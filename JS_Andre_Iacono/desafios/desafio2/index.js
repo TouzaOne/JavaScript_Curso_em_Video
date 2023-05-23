6bb781ed0db7145e0816c6520c5401e6511cef9f
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-23 20:18:54 -0300

    adicionando style ao desafio

--- JS_Andre_Iacono/desafios/desafio2/index.js
+++ JS_Andre_Iacono/desafios/desafio2/index.js
@@ -2,6 +2,10 @@ let value = Number(prompt('Enter the product value: '))
 
 if (value >= 20) {
     document.getElementById('result').innerHTML = 'Approved!!'
+    let a = document.getElementById('result')
+    a.style.background = 'lightgreen'
 } else {
     document.getElementById('result').innerHTML = 'Denied!!'
+    let d = document.getElementById('result')
+    d.style.background = 'red'
 }
