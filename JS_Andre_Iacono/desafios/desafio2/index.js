ef4502ea45f0c62e88bdd48c9b60ee2990593a14
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-23 19:57:46 -0300

    adicionando script ao desafio

--- JS_Andre_Iacono/desafios/desafio2/index.js
+++ JS_Andre_Iacono/desafios/desafio2/index.js
@@ -1,2 +1,7 @@
 let value = Number(prompt('Enter the product value: '))
-console.log(value)
+
+if (value >= 20) {
+    document.getElementById('result').innerHTML = 'Approved!!'
+} else {
+    document.getElementById('result').innerHTML = 'Denied!!'
+}
