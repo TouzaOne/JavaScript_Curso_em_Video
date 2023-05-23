cf0a850f665a0ee759ce13d0adfab67d3b3aa183
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-23 18:16:34 -0300

    BUILT IN em JS

---
+++ JS_Andre_Iacono/exercicios/built-in-string.js
@@ -0,0 +1,11 @@
+// Procure conhecer os diversos tipos de BUILT IN que o JavaScript possui em sua documentação (developer.mozilla.org), pois eles lhe ajudarão, muito em breve, na execução do seus códigos...
+
+let msg = 'Hey, my name is Tiago'
+
+console.log(msg.startsWith('Hey')) // Confere se o que vc digitou bate com o conteúdo inicial da variável em questão.
+
+console.log(msg.length) // Diz a quantidade de caracteres existentes na variável em questão... (espaços em branco, também são contabilizados)
+
+console.log(msg.includes('meu')) // Verifica se o valor que vc digitou existe dentro da variável em questão ou não...
+
+console.log(msg.endsWith('go')) // Confere se o que vc digitou bate com o conteúdo final da variável em questão.
