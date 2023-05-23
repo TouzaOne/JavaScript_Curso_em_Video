3524f7b784e1db3c8f8d7678f5b80097ec265184
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-23 17:37:00 -0300

    FACTORY em JS

---
+++ JS_Andre_Iacono/exercicios/factory.js
@@ -0,0 +1,19 @@
+// Factory nada mais é que uma fábrica de objetos...
+
+function createBook(title, author, pages) { // factory
+    const book = { // object
+        bookTitle: title,
+        bookAuthor: author,
+        bookPages: pages, 
+        printBook: function() {
+            console.log('Printing...')
+        }
+    }
+    return book
+}
+
+const book1 = createBook('Atomic', 'James', 306)
+const book2 = createBook('Think', 'Napolean', 450)
+
+console.log(book1)
+console.log(book2)
