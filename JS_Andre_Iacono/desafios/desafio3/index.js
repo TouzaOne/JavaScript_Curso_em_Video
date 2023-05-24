1659fd33a0a50ac5b6f967bdf80fe79cfa5fb65e
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-24 18:29:00 -0300

    fazendo a parte de html e css

--- JS_Andre_Iacono/desafios/desafio3/index.js
+++ JS_Andre_Iacono/desafios/desafio3/index.js
@@ -1,14 +1,1 @@
 
-let value = Number(prompt('Enter the product value: '))
-
-if (value >= 20) {
-    document.getElementById('result').innerHTML = 'Approved!!'
-    let a = document.getElementById('result')
-    a.style.color = 'blue'
-    a.style.background = 'lightgreen'
-} else {
-    document.getElementById('result').innerHTML = 'Denied!!'
-    let d = document.getElementById('result')
-    d.style.color = 'yellow'
-    d.style.background = 'red'
-}
