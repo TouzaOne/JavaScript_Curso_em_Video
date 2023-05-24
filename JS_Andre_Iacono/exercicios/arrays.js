bb23150b7855104d2092d7f46d0e1b2d5ac04349
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-24 09:31:05 -0300

    adicionando elementos numa array

--- JS_Andre_Iacono/exercicios/arrays.js
+++ JS_Andre_Iacono/exercicios/arrays.js
@@ -15,9 +15,16 @@ console.log(friends)
 friends[3] = 'Tiago'
 console.log(friends)
 
+// Adicionar um elemento novo no início...
+friends.unshift(1, 7, 9)
+console.log(friends)
+
+// Adicionar novos elementos no meio de uma array requer um pouco mais de atenção pelo seguinte: 1º vc deve informar o índice a partir do qual, os novos elementos serão acrescidos; 2º vc deve informar se vão haver decrementos da sua lista atual, antes do acréscimo dos novos elementos; e, 3º digitar os novos valores que serão acrescidos à sua array. 
+friends.splice(3, 0, 4, 5, 6)
+console.log(friends)
+
 // Adicionar um elemento novo na última vaga...
-friends.push(12)
-friends.push(3)
+friends.push(3, 2)
 console.log(friends)  
 
 // Colocar os elementos em ordem alfabética ou crescente (se forem números)
