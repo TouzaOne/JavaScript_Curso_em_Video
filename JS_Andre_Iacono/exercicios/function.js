5337e4b03937a1d142096c9b6dcfff81fa678376
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-25 09:03:12 -0300

    funcionamento do hoisting dentro do JS

--- JS_Andre_Iacono/exercicios/function.js
+++ JS_Andre_Iacono/exercicios/function.js
@@ -35,3 +35,31 @@ car() // Note que aqui, ao invés de chamar a função, nós chamamos a variáve
 const truck = car
 
 truck() // Execute, e veja que o resultado é mesmo que o resultado de cima.
+
+// HOISTING: Vale destacar aqui o processo que o JavaScript utiliza ao lidar com os comandos passados nos seus códigos. O JS sempre executará as funções em primeiro lugar, para depois dar continuidade ao restante dos comandos. E, nesse processo, dependendo do tipo de function que vc estiver utilizando, isso sim, implicará no resultado do seu programa, podendo dar erro em uma situação específica... Veja:
+
+// Utilizando a Function Declaration...
+function movie() {  // Veja que 1º é executada a função...
+    console.log('The Matrix')
+}
+movie() // E, em 2º lugar é executada o chamado dessa função...
+
+// Por isso, independentemente, de como escrevemos o nosso comando, na Function Declaration, o processo HOISTING sempre executará as funções primeiro... Veja:
+movie()
+
+function movie() {
+    console.log('The Matrix')
+}
+
+// Isso já não ocorre quando utilizamos a Function Expression, pois a função está imbutida dentro de uma variável, por isso, o processo HOISTING não consegue encontrar a função e quando nós mudamos a localização dos comandos no código, o programa pode gerar um erro. Veja...
+
+const bike = function() {
+    console.log('Caloi')
+}
+bike() // Dessa forma, funciona!!! Já, do jeito abaixo, vai dar erro ao ser executado o programa... Veja...
+
+baike()
+const baike = function() {
+    console.log('Caloi')
+}
+
