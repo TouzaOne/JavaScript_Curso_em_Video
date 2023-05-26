3baa575002cc61687f3fe59cf57b2be12c6ad1e2
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-26 08:39:20 -0300

    adicionando cálculos ao projeto

--- JS_Andre_Iacono/desafios/desafio4/index.js
+++ JS_Andre_Iacono/desafios/desafio4/index.js
@@ -2,4 +2,10 @@ function update() {
     let bill = Number(document.getElementById('yourBill').value)
     let tipPercent = document.getElementById('tipInput').value
     let split = document.getElementById('splitInput').value
+
+    let tipValue = bill * (tipPercent / 100)
+    let billTotal = bill + tipValue
+    let billEach = billTotal / split
+    
+
 }
