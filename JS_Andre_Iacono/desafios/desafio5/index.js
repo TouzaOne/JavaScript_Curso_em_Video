8a7412f8f1288a469c1e9482bd31be4a31db6c7a
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-26 11:07:38 -0300

    adicionando mais alguns efeitos

--- JS_Andre_Iacono/desafios/desafio5/index.js
+++ JS_Andre_Iacono/desafios/desafio5/index.js
@@ -1,5 +1,5 @@
 
-const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
+const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green', 'Pink', 'Orange', 'Brown', 'Aqua']
 const btn = document.getElementById('btn')
 const color = document.querySelector('.color')
 
@@ -8,6 +8,8 @@ btn.addEventListener('click', function() {
     const randomColor = getRandomColor()
     document.body.style.background = colors[randomColor] // aplicando a cor ao background
     color.textContent = colors[randomColor]
+    document.querySelector('.color').style.color = colors[randomColor]
+    document.getElementById('h1').style.color = 'darkgray'  
 })
 
 // Essa é a função responsável por criar os números randômicos...
