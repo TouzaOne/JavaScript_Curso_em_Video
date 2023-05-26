3ce998f780d0dbb4816561123bac200bcf8d6878
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-26 10:03:01 -0300

    criando uma sequência randômica para as cores

--- JS_Andre_Iacono/desafios/desafio5/index.js
+++ JS_Andre_Iacono/desafios/desafio5/index.js
@@ -4,4 +4,10 @@ const btn = document.getElementById('btn')
 
 btn.addEventListener('click', function() {
     document.body.style.background = colors[0]
+    const randomColor = getRandomColor()
+
 })
+
+function getRandomColor() {
+    return Math.floor(Math.random() * colors.length)
+}
