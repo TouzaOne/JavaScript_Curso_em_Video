d578b6c7f550bc78ca58637ea644c120857528a0
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-27 14:34:39 -0300

    adicionando mais funções

--- JS_Andre_Iacono/desafios/desafio6/index.js
+++ JS_Andre_Iacono/desafios/desafio6/index.js
@@ -1,6 +1,13 @@
 
 let computerNumber
+let userNumbers = []
 
 function init() {
     computerNumber = Math.floor(Math.random() * 100 + 1)
+}
+
+function compareNumbers() {
+    const useNumber = Number(document.getElementById('inputBox').value)
+    userNumbers.push(' ' + userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
 }
