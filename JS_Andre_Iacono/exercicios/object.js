47eb8749fbc3d93d7ad294dba686586f430ad255
TouzaOne <ogaitsilvasouza@gmail.com>
2023-05-30 22:37:37 -0300

    adicionando função + objeto dentro do exercício

--- JS_Andre_Iacono/exercicios/object.js
+++ JS_Andre_Iacono/exercicios/object.js
@@ -89,3 +89,23 @@ nascimento.mes = 7
 
 console.log(`${nascimento.nome} nasceu em ${nascimento.exibir()} no estado do Rio de Janeiro!`)
 
+// Também podemos usar uma função e dentro dela colocar um ou vários objetos..
+
+function criarData(dia, mes, ano) {
+    return {
+        dia: dia,
+        mes: mes,
+        ano: ano,
+        exibir: function() {
+            return `${this.dia}/${this.mes}/${this.ano}`
+        }
+    }
+}
+
+const d1 = criarData(24, 03, 1984)
+const d2 = criarData(24, 07, 1984)
+const d3 = criarData(09, 07, 2010)
+
+console.log(d1.exibir())
+console.log(d2.exibir())
+console.log(d3.exibir())
