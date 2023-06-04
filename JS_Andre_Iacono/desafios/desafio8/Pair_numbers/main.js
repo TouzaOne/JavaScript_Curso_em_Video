9dda3d1494666eaa0553d86183842b37a43880ba
TouzaOne <ogaitsilvasouza@gmail.com>
2023-06-03 21:29:22 -0300

    adicionando conteúdo

--- JS_Andre_Iacono/desafios/desafio8/Pair_numbers/main.js
+++ JS_Andre_Iacono/desafios/desafio8/Pair_numbers/main.js
@@ -1,19 +1,24 @@
 let startInput = document.getElementById('start')
 let endInput = document.getElementById('end')
 let outputDiv = document.getElementById('output')
+let listPutDiv = document.getElementById('listput')
 
 function showEvenNumbers() {
     let start = Number(startInput.value)
     let end = Number(endInput.value)
 
     let i = start
-    let output = 0
+    let output = 0    
+    let listput = [0]
 
     while (i <= end) {
         if (i % 2 == 0) {
-        output = output + i + ' '
-        outputDiv.innerHTML = output
-        }
-        i++  
+        output = output + i + ' '         
+        outputDiv.innerHTML = output        
+        }        
+        i++ 
+        listput = output
+        listPutDiv.innerHTML = `A sequência têm ${listput.length} números pares`
     }
+    
 }
