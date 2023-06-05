b5f15e64b613b06dab36096e633d077db2751c7c
TouzaOne <ogaitsilvasouza@gmail.com>
2023-06-05 10:23:36 -0300

    adicionando conteúdo ao desafio

--- JS_Andre_Iacono/desafios/desafio11/main.js
+++ JS_Andre_Iacono/desafios/desafio11/main.js
@@ -1,10 +1,17 @@
 let num = document.getElementById('num')
 let resultNumber = document.getElementById('result')
-let i = 0
+let checkNumber = document.getElementById('check')
 
-function shomNumber() {
-    if (num >= 0 && num <= 10) {
-        resultNumber = num
+checkNumber.addEventListener('click', function() {
+    let number = Number(num.value)
+    
+    if (number >= 1 && number <= 10) {
         resultNumber.innerHTML = 'The number is valid!!'
+    } else if (number < 1) {
+        resultNumber.innerHTML = 'The number is invalid!!'
+    } else if (number > 10) {
+        resultNumber.innerHTML = 'The number is invalid!!'
     }
-}
+
+    num.value = ''
+})
