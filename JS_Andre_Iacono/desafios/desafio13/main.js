37ffbdc371a6cf3714ca9a933efbce4c3fe50c37
TouzaOne <ogaitsilvasouza@gmail.com>
2023-06-06 20:53:13 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio13/main.js
+++ JS_Andre_Iacono/desafios/desafio13/main.js
@@ -5,3 +5,14 @@ const images = [
     'images/pega_visao.png'
 ]
 
+let imageElement = document.getElementById('image')
+let buttonElement = document.getElementById('button')
+let currentIndex = 0
+
+buttonElement.addEventListener('click', function() {
+    currentIndex++
+    if (currentIndex >= images.length) {
+        currentIndex = 0
+    }
+    imageElement.src = images[currentIndex]
+})
