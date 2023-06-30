e4ad1a44ca1c368dfa3f108d89a131ccbafe2320
TouzaOne <ogaitsilvasouza@gmail.com>
2023-06-30 17:15:24 -0300

    Update main.js

---
+++ JS_Andre_Iacono/desafios/desafio25/main.js
@@ -0,0 +1,9 @@
+const calculateButton = document.getElementById('calculate');
+calculateButton.addEventListener('click', function() {
+    const carPrice = document.getElementById('price').value;
+    const downPayment = document.getElementById('downPayment').value;
+    const loanTerm = document.getElementById('loanTerm').value;
+    const interestRate = document.getElementById('interestRate').value;
+
+    
+})
