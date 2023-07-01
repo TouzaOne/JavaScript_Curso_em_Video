29129f121d143143e936292aa06dd8d90dfd408c
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-01 11:14:04 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio26/main.js
+++ JS_Andre_Iacono/desafios/desafio26/main.js
@@ -1,5 +1,5 @@
 document.querySelector('#push').onclick = function() {
-    if (document.querySelector(#newtask input).value.length == 0) {
+    if (document.querySelector('#newtask input').value.length == 0) {
         alert('Kindly Enter Task Name!!!')
     } else {
         document.querySelector('#tasks').innerHTML += 
