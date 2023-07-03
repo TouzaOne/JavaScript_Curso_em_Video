739a6c757644e5727ca850ebb3c0f41ece7b9707
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-03 10:17:31 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio27/main.js
+++ JS_Andre_Iacono/desafios/desafio27/main.js
@@ -5,3 +5,14 @@ const calculator = {
     operator: null,
 };
 
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+}
+
