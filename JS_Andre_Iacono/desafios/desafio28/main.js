9d04a4929d530697788e5def2814dd8b7ab645c4
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-04 17:22:27 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio28/main.js
+++ JS_Andre_Iacono/desafios/desafio28/main.js
@@ -2,3 +2,7 @@ const openModalButton = document.getElementById('openModal');
 const modal = document.getElementById('modal');
 const closeModalButton = document.getElementsByClassName('close')[0];
 
+openModalButton.addEventListener('click', function() {
+    modal.style.display = 'block'; // show the modal
+});
+
