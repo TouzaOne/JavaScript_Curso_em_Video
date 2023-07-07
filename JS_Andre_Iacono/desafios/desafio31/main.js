73070eea1456f66ab1b06b01ce376b80fb5415ed
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-07 09:38:44 -0300

    Update main.js

---
+++ JS_Andre_Iacono/desafios/desafio31/main.js
@@ -0,0 +1,9 @@
+function calculate() {
+    var bmi;
+    var result = document.getElementById('result');
+
+    var weight = parseInt(document.getElementById('weight').value);
+    document.getElementById('weight-val').textContent = weight + ' kg';
+
+    
+}
