5149f3baf68690459fee5eb20bb40ce0f244b8b4
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-08 09:48:32 -0300

    Update main.js

---
+++ JS_Andre_Iacono/desafios/desafio32/main.js
@@ -0,0 +1,7 @@
+const clockElement = document.getElementById('clock');
+
+function updateClock() {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');
+    
+}
