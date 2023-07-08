1fefd08d9459a693c0823aa8e500e13f91f1b1a9
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-08 09:51:23 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio32/main.js
+++ JS_Andre_Iacono/desafios/desafio32/main.js
@@ -5,5 +5,8 @@ function updateClock() {
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
     const seconds = date.getSeconds().toString().padStart(2, '0');
-    
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 }
+
+updateClock();
+setInterval(updateClock, 1000);
