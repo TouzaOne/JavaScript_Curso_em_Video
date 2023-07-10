c20efccb0c1a47e733e4adc8f61704f83a878454
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-10 10:08:35 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio34/main.js
+++ JS_Andre_Iacono/desafios/desafio34/main.js
@@ -24,5 +24,29 @@ for (let i = 2; i > 0; i--) {
     selectMenu[2].firstElementChild.insertAdjacentHTML('afterend', option);
 }
 
+setInterval(() => {
+    let date = new Date(),
+    h = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds(),
+    ampm = 'AM';
+
+    if (h >= 12) {
+        h = h - 12;
+        ampm = 'PM';
+    }
+
+    h = h == 0 ? h = 12 : h;
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
+    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+
+    if (alarmTime === `${h}:${m} ${ampm}`) {
+        ringtone.play();
+        ringtone.loop = true;
+    }
+});
+
 
 
