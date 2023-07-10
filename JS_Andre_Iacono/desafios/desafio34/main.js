2113f31bb3dd3b44e243680ec45e135924eebb42
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-10 10:02:28 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio34/main.js
+++ JS_Andre_Iacono/desafios/desafio34/main.js
@@ -7,7 +7,7 @@ let alarmTime, isAlarmSet,
 ringtone = new Audio('./sounds/ringtone.mp3');
 
 for (let i = 12; i > 0; i--) {
-    i = 1 < 10 ? `0${i}` : i;
+    i = i < 10 ? `0${i}` : i;
     let option = `<option value='${i}'>${i}</option>`;
     selectMenu[0].firstElementChild.insertAdjacentHTML('afterend', option);
 }
@@ -23,4 +23,6 @@ for (let i = 2; i > 0; i--) {
     let option = `<option value='${ampm}'>${ampm}</option>`;
     selectMenu[2].firstElementChild.insertAdjacentHTML('afterend', option);
 }
+
+
 
