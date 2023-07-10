dac7fee3caff73168e93e26a47610457dae388f9
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-10 09:48:27 -0300

    Update main.js

---
+++ JS_Andre_Iacono/desafios/desafio34/main.js
@@ -0,0 +1,5 @@
+const currentTime = document.querySelector('h1'),
+content = document.querySelector('.content'),
+selectMenu = document.querySelectorAll('select'),
+setAlarmBtn = document.querySelector('button');
+
