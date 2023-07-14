5d32321af8efc47734e1bd87dd2f1d1e8b97071d
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-14 15:25:55 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio38/main.js
+++ JS_Andre_Iacono/desafios/desafio38/main.js
@@ -4,7 +4,8 @@ box.addEventListener('mousemove', (e) => {
     let y = e.offsetY
     let BoxWidth = box.clientWidth
     let BoxHeight = box.clientHeight
-    let moveX = (x - BoxWidth)
-    let moveY = (y - BoxHeight)
-    box.style.transform = `translateX(${moveX}px)`;
+    let moveX = (x - BoxWidth/2)
+    let moveY = (y - BoxHeight/2)
+    box.style.transform = `translateX(${moveX}px) translateY(${moveY}px)`;
 })
+
