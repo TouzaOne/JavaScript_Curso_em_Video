1149dca7beca3925aa3b3aa747cc1ea94ad7ff87
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-14 15:22:16 -0300

    Update main.js

---
+++ JS_Andre_Iacono/desafios/desafio38/main.js
@@ -0,0 +1,8 @@
+let box = document.querySelector('.box');
+box.addEventListener('mousemove', (e) => {
+    let x = e.offsetX
+    let y = e.offsetY
+    let BoxWidth = box.clientWidth
+    let BoxHeight = box.clientHeight
+    
+})
