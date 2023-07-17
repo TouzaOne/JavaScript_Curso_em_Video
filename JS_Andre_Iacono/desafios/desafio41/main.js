7f0b654ce95b72ab92b30a1c3bf52e3af5cad176
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-17 10:34:43 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio41/main.js
+++ JS_Andre_Iacono/desafios/desafio41/main.js
@@ -1,8 +1,11 @@
+let sizeW = Math.random() * 3;
+
 function stripes() {
     let e = document.createElement('div');
     e.classList.add('lines');
     document.body.appendChild(e);
     e.style.left = Math.random() * innerWidth + 'px';
+    e.style.width = sizeW + 'px';
 
     setTimeout(function() {
         document.body.removeChild(e)
