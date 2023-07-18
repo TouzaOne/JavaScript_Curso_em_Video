71c765854b43ce20908895b89c73646123607430
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-18 14:11:20 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio42/main.js
+++ JS_Andre_Iacono/desafios/desafio42/main.js
@@ -3,5 +3,11 @@ let nav = document.querySelector('nav');
 let item = document.querySelectorAll('nav a');
 
 nav.onclick = function() {
-    
+    marker.classList.toggle('change');
+}
+
+function indicator(e) {
+    marker.style.left = e.offsetLeft + 'px';
+    marker.style.width = e.offsetWidth + 'px';
+    marker.style.display = 'block';
 }
