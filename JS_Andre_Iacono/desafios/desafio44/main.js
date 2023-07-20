6db4e0702ccd8db41317261863a76f69bd9107a1
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-20 09:52:01 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio44/main.js
+++ JS_Andre_Iacono/desafios/desafio44/main.js
@@ -1,1 +1,9 @@
 const emojis = ["😀","😀","😉","😉","🤪","🤪","🤢","🤢","😡","😡","🥳","🥳","🤯","🤯","🤫","🤫"];
+
+var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+for (var i = 0; i < emojis.length; i++) {
+    let box = document.createElement('div')
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i]
+    document.querySelector('.game').appendChild(box);
+}
