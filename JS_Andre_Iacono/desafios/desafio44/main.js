c30ff385066621370a990c023dce804c92ba8170
TouzaOne <ogaitsilvasouza@gmail.com>
2023-07-20 10:06:04 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio44/main.js
+++ JS_Andre_Iacono/desafios/desafio44/main.js
@@ -5,5 +5,8 @@ for (var i = 0; i < emojis.length; i++) {
     let box = document.createElement('div')
     box.className = 'item';
     box.innerHTML = shuf_emojis[i]
+    box.onclick = function() {
+        this.classList.add('boxOpen')
+    }
     document.querySelector('.game').appendChild(box);
 }
