932a6e736542a8550a23b26b079d3b10c81b6df7
TouzaOne <ogaitsilvasouza@gmail.com>
2023-08-05 19:41:12 -0300

    criando conteúdo

---
+++ JS_Andre_Iacono/desafios/desafio57/main.js
@@ -0,0 +1,13 @@
+const product = [
+    {
+        id: 1,
+        image: 'image/gg-1.jpg',
+        title: 'Z Flip Foldable Mobile',
+        price: 120,
+        category: 'mobile'
+    },
+    {
+        id: 1,
+    }
+
+]
