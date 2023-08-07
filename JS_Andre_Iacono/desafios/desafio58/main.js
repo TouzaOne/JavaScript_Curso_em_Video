6fe35db01054be9ae6e67b2926bbe3c884a722e7
TouzaOne <ogaitsilvasouza@gmail.com>
2023-08-07 18:29:54 -0300

    add comand

--- JS_Andre_Iacono/desafios/desafio58/main.js
+++ JS_Andre_Iacono/desafios/desafio58/main.js
@@ -1,3 +1,3 @@
 let label = document.querySelector('label');
 
-label.innerHTML = label.innerText.split('').map((letters, i)=> `<span>${letters}</span>`).join('');
+label.innerHTML = label.innerText.split('').map((letters, i)=> `<span style='transition-delay: ${i*30}ms'>${letters}</span>`).join('');
