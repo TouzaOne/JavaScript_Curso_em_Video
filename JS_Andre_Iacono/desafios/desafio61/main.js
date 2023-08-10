939652dcae82329a240c0a5895438ad089cf405a
TouzaOne <ogaitsilvasouza@gmail.com>
2023-08-10 03:10:25 -0300

    Update main.js

---
+++ JS_Andre_Iacono/desafios/desafio61/main.js
@@ -0,0 +1,10 @@
+var textarea = document.querySelector('textarea');
+textarea.addEventListener('keydown', autosize);
+
+function autosize() {
+var el = textarea;
+setTimeout(function(){
+el.style.cssText = 'height:auto; padding:0';
+el.style.cssText = 'height:' + el.scrollHeight + 'px';
+},0);
+}
