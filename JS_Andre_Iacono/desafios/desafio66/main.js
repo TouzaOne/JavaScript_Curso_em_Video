dfe631ad1d37cbc937e2dd87c406d4359783f092
TouzaOne <ogaitsilvasouza@gmail.com>
2023-08-17 15:25:37 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio66/main.js
+++ JS_Andre_Iacono/desafios/desafio66/main.js
@@ -8,8 +8,24 @@ t = 0
 T = Math.tan
 rsz = window.onresize=()=> {
     setTimeout(()=> {
-        if(document.body.clientWidth > document.body.clientHeight*1.77777778)
-    } 
+        if(document.body.clientWidth > document.body.clientHeight*1.77777778){ 
     c.style.height = '100vh'
-    setTimeout(()=> c.style.width = c.clientHeight*1.77777778+'px', 0))
+    setTimeout(()=> c.style.width = c.clientHeight*1.77777778+'px', 0)
+    } else {
+        c.style.width = '100vw'
+        setTimeout(()=> c.style.height = c.clientWidth/1.77777778 + 'px',0)
+    }
+    },0)
+}
+rsz()
+async function Draw() {
+    if(!t) {
+        R= (Rl, Pt, Yw, m)=> {
+            M=Math
+            A=M.atan2
+            H=M.hypot
+            X=S(p=A(x,y)+Rl)*(d=H(X,Y))
+            Y=C(p)*
+        }
+    }
 }
