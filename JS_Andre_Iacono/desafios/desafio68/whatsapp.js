47ada6d636554f3fab3df9440d5d4d2cfe061af9
TouzaOne <ogaitsilvasouza@gmail.com>
2023-08-21 15:06:40 -0300

    Create whatsapp.js

---
+++ JS_Andre_Iacono/desafios/desafio68/whatsapp.js
@@ -0,0 +1,8 @@
+function sendToWhatsapp() {
+    let number = "+5522999685258";
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    
+}
