9794b3f987a138416131233230b55983013b698b
TouzaOne <ogaitsilvasouza@gmail.com>
2023-08-21 15:09:42 -0300

    Update whatsapp.js

--- JS_Andre_Iacono/desafios/desafio68/whatsapp.js
+++ JS_Andre_Iacono/desafios/desafio68/whatsapp.js
@@ -4,5 +4,10 @@ function sendToWhatsapp() {
     let email = document.getElementById('email').value;
     let message = document.getElementById('message').value;
 
+    var url = "https://wa.me/" + number + "?text=" 
+    + "Name : " +name+ "%0a"
+    + "Email : " +email+ "%0a"
+    + "Message : " +message+ "%0a%0a";
+
     
 }
