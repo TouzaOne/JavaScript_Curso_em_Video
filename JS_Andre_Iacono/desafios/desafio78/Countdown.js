24ac6d71db4340a6f082355453d06ae00c4bb4cb
TouzaOne <ogaitsilvasouza@gmail.com>
2023-09-04 08:50:36 -0300

    Update Countdown.js

--- JS_Andre_Iacono/desafios/desafio78/Countdown.js
+++ JS_Andre_Iacono/desafios/desafio78/Countdown.js
@@ -6,3 +6,14 @@ timer = setInterval(function () {
     timeBetweenDates(compareDate);
 }, 1000);
 
+function timeBetweenDates(toDate) {
+    var dateEntered = toDate;
+    var now = new Date();
+    var difference = dateEntered.getTime() - now.getTime();
+
+    if(difference <= 0) {
+        clearInterval(timer);
+    } else {
+        
+    }
+}
