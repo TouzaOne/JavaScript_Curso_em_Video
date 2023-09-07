783403701cff45ceca9406283852a701e1bdce51
TouzaOne <ogaitsilvasouza@gmail.com>
2023-09-07 15:33:49 -0300

    Update main.js

--- JS_Andre_Iacono/desafios/desafio81/main.js
+++ JS_Andre_Iacono/desafios/desafio81/main.js
@@ -4,3 +4,18 @@ for (let i = 1; i <= 900; i++) {
     dot.classList.add('element');
     container.appendChild(dot);
 }
+
+let grid = [30,30];
+
+let dotAll = document.querySelectorAll('.element');
+let animation = anime.timeline({
+    targets: dotAll,
+    easing: 'easeInOutExpo',
+    loop: true,
+})
+
+animation
+.add({
+    rotate: function() {
+        return anime.random(-360,360)}
+})
