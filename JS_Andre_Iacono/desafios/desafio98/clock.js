9abe9a70a2f71b18d9cb4cc3f6776204ab08ca7b
TouzaOne <ogaitsilvasouza@gmail.com>
2023-10-01 09:47:44 -0300

    Update clock.js

--- JS_Andre_Iacono/desafios/desafio98/clock.js
+++ JS_Andre_Iacono/desafios/desafio98/clock.js
@@ -1,5 +1,9 @@
 function clock() {
+
     var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var today = new Date();
+
+    document.getElementById('Date').innerHTML = (dayNames[today.getDay()] + " " + today.getDate() + ' ' + monthNames[today.getMonth()] + ' ' + today.getFullYear());
 
 } var inter = setInterval(clock, 400);
