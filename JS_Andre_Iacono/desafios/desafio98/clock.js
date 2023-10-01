ff5ca93f869589fd9adc5f6f333a0fb7ea9b394b
TouzaOne <ogaitsilvasouza@gmail.com>
2023-10-01 09:52:01 -0300

    Update clock.js

--- JS_Andre_Iacono/desafios/desafio98/clock.js
+++ JS_Andre_Iacono/desafios/desafio98/clock.js
@@ -11,6 +11,8 @@ function clock() {
     var s = today.getSeconds();
     var day = h<11 ? 'AM': 'PM';
 
-    h = h<10 ?
+    h = h<10 ? '0' +h: h;
+    m = m<10 ? '0' +m: m;
+    s = s<10 ? '0' +s: s;
 
 } var inter = setInterval(clock, 400);
