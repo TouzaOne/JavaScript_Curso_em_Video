8af5899b90a745cd03627cc891074f2fb2311c26
TouzaOne <ogaitsilvasouza@gmail.com>
2023-10-01 09:42:56 -0300

    Create clock.js

---
+++ JS_Andre_Iacono/desafios/desafio98/clock.js
@@ -0,0 +1,5 @@
+function clock() {
+    var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+} var inter = setInterval(clock, 400);
